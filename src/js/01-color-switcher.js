2d1728fe04ecfc53b2817325a714b586d23c9571
direct66612 <direct66612@gmail.com>
2023-09-21 04:30:02 -0400

    task 1 completed

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,20 @@
-
+const startbtn = document.querySelector('button[data-start]');
+const stopbtn = document.querySelector('button[data-stop]');
+let idInterval;
+startbtn.addEventListener('click', function () {
+  this.disabled = true;
+  idInterval = setInterval(() => {
+    document.body.style.background = getRandomHexColor();
+  }, 1000);
+  stopbtn.disabled = false;
+});
+stopbtn.addEventListener('click', function () {
+  this.disabled = true;
+  clearInterval(idInterval);
+  startbtn.disabled = false;
+});
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
